9451a18901f0d8a57b8ed2cc1efbce70090f37e0
Rosen Svilenov <rosen.svilenov@gmail.com>
2022-03-09 14:01:55 +0200

    the type extensions are now in a separate class

--- src/FluentAssertions.Properties/ObjectExtensions.cs
+++ src/FluentAssertions.Properties/ObjectExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using FluentAssertions.Properties.Common;
 using FluentAssertions.Properties.Extensions;
 using FluentAssertions.Properties.Selectors;
-using FluentAssertions.Types;
 using System;
 using System.Diagnostics;
 using System.Linq;
@@ -15,28 +14,6 @@ namespace FluentAssertions.Properties
     [DebuggerNonUserCode]
     public static class ObjectExtensions
     {
-        /// <summary>
-        /// Returns a property selector for the current <see cref="Type"/>. 
-        /// This method directs the call to the original FluentAssertions library.
-        /// </summary>
-        /// <exception cref="ArgumentNullException"><paramref name="type"/> is <c>null</c>.</exception>
-        public static PropertyInfoSelector Properties(this Type type)
-        {
-            return new PropertyInfoSelector(type);
-        }
-
-        /// <summary>
-        /// Returns a property selector for the current <see cref="Type"/>.
-        /// This method directs the call to the original FluentAssertions library.
-        /// </summary>
-        /// <exception cref="ArgumentNullException"><paramref name="typeSelector"/> is <c>null</c>.</exception>
-        public static PropertyInfoSelector Properties(this TypeSelector typeSelector)
-        {
-            Guard.ThrowIfArgumentIsNull(typeSelector, nameof(typeSelector));
-
-            return new PropertyInfoSelector(typeSelector.ToList());
-        }
-
         /// <summary>
         /// Returns a property selector for the current instance.
         /// </summary>
---
+++ src/FluentAssertions.Properties/TypeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions.Properties.Common;
+using FluentAssertions.Types;
+using System;
+using System.Linq;
+
+namespace FluentAssertions.Properties
+{
+    /// <summary>
+    /// Extension methods for getting property selectors for a type.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Returns a property selector for the current <see cref="Type"/>. 
+        /// This method directs the call to the original FluentAssertions library.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="type"/> is <c>null</c>.</exception>
+        public static PropertyInfoSelector Properties(this Type type)
+        {
+            return new PropertyInfoSelector(type);
+        }
+
+        /// <summary>
+        /// Returns a property selector for the current <see cref="Type"/>.
+        /// This method directs the call to the original FluentAssertions library.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"><paramref name="typeSelector"/> is <c>null</c>.</exception>
+        public static PropertyInfoSelector Properties(this TypeSelector typeSelector)
+        {
+            Guard.ThrowIfArgumentIsNull(typeSelector, nameof(typeSelector));
+
+            return new PropertyInfoSelector(typeSelector.ToList());
+        }
+    }
+}
